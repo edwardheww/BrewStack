ea68a398529cb4bbc501bf8c42741acd912668f7
Edward Hew <edwardhewyc@gmail.com>
2026-05-27 16:20:54 +0800

    Saving changes before pulling

--- src/scraper/scheduler.ts
+++ src/scraper/scheduler.ts
@@ -2,10 +2,9 @@ import * as cron from 'node-cron';
 import { HomegroundScraper } from './scrapers/HomegroundScraper.js';
 import { Roaster } from './types/index.js';
 import { upsertScrapedBeans } from '../db/upsert.js';
-import { error } from 'node:console';
 
 export function registerScraperCron() {
-    cron.schedule('0 0 * * *', async () => {
+    cron.schedule('* * * * *', async () => {
         console.log('Running daily scraping.');
         const roaster = new Roaster('hg', 'HomeGround', 'https://homegroundcoffeeroasters.com/collections/coffees-specialty');
         const hgscraper = new HomegroundScraper(roaster);
