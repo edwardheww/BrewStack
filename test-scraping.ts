9b920ebb6f97558b8bd0ec7afa854006c6d8d470
Edward <94817926+edwardheww@users.noreply.github.com>
2026-05-29 21:58:13 +0800

    Deleted test-scraping.ts

--- test-scraping.ts
+++
@@ -1,7 +0,0 @@
-import { NylonScraper } from "./src/scraper/scrapers/NylonScraper.js";
-import { Roaster } from './src/scraper/types/index.js';
-
-const roaster = new Roaster('nyl', 'Nylon', 'https://nylon.coffee/collections/coffee');
-const scraper = new NylonScraper(roaster);
-const result = await scraper.run();
-console.log(result);
