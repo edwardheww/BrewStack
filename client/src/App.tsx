76637698e74cd1f97556a4281f72f799c87d8f6f
Edward Hew <edwardhewyc@gmail.com>
2026-06-04 17:25:50 +0800

    Fixed app.tsx routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
+import Catalog from "./pages/Catalog";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/catalog" element={<Catalog />} />
                 <Route path="/" element={<Navigate to="/catalog" />} />
             </Routes>
         </BrowserRouter>
