fd0e7698961310d15ade93e264eedd1404e29ce6
Edward Hew <edwardhewyc@gmail.com>
2026-06-23 20:32:22 +0800

    updated map texture & faulty marker icons

--- client/src/components/RoasterMap.tsx
+++ client/src/components/RoasterMap.tsx
@@ -1,5 +1,13 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 
 const ROASTERS = [
     { name: 'Homeground Coffee Roasters', lat: 1.3332, long: 103.7890, address: '911 Bukit Timah Rd' },
@@ -11,7 +19,7 @@ const ROASTERS = [
 export default function RoasterMap() {
     return (
         <MapContainer center={[1.3521, 103.8198]} zoom={12} style={{ height: '400px', width: "100%" }}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap contributors" />
+            <TileLayer url="https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap contributors" />
             {
                 ROASTERS.map(roaster => (
                     <Marker key={roaster.name} position={[roaster.lat, roaster.long]}>
