199e01071c92922dbc6bd497cee9a3a97f46d9bf
Edward Hew <edwardhewyc@gmail.com>
2026-06-23 20:42:26 +0800

    Fixing map markers rendering error

--- client/src/components/RoasterMap.tsx
+++ client/src/components/RoasterMap.tsx
@@ -1,13 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-
-L.Icon.Default.mergeOptions({
-    iconUrl: markerIcon,
-    shadowUrl: markerShadow,
-});
+import { icon } from 'leaflet';
 
 const ROASTERS = [
     { name: 'Homeground Coffee Roasters', lat: 1.3332, long: 103.7890, address: '911 Bukit Timah Rd' },
@@ -16,13 +9,22 @@ const ROASTERS = [
     { name: 'Alchemist Coffee', lat: 1.3023, long: 103.8376, address: '260 Orchard Rd' },
 ]
 
+const defaultIcon = icon({
+    iconUrl: '/marker-icon.png',
+    iconRetinaUrl: '/marker-icon-2x.png',
+    shadowUrl: '/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+})
+
 export default function RoasterMap() {
     return (
         <MapContainer center={[1.3521, 103.8198]} zoom={12} style={{ height: '400px', width: "100%" }}>
             <TileLayer url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png" attribution="&copy; OpenStreetMap contributors" />
             {
                 ROASTERS.map(roaster => (
-                    <Marker key={roaster.name} position={[roaster.lat, roaster.long]}>
+                    <Marker key={roaster.name} position={[roaster.lat, roaster.long]} icon={defaultIcon}>
                         <Popup>{roaster.name}</Popup>
                     </Marker>
                 ))
